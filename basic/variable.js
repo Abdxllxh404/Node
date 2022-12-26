5c5b01b6f0a001a53466df6ad1262cec9c850d7c
Abdulloh404 <adlforgithub@gmail.com>
2022-12-26 13:54:34 +0700

    Variable

---
+++ basic/variable.js
@@ -0,0 +1,11 @@
+let number = 123;
+let stri = 'hello';
+let ecall = false;
+
+console.log('Hello World');
+console.log(typeof number);
+console.log(typeof stri);
+console.log(typeof ecall);
+console.log(typeof null);
+console.log(typeof a);
+console.log(typeof console.log);
