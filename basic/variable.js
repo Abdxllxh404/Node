12489faebea0d89a20742ed88145fb0d0aef25c2
Abdulloh404 <adlforgithub@gmail.com>
2022-12-27 11:00:38 +0700

    VAR

--- basic/variable.js
+++ basic/variable.js
@@ -1,6 +1,7 @@
 let number = 123;
 let stri = 'hello';
 let ecall = false;
+const TELL = 0826523066;
 
 console.log('Hello World');
 console.log(typeof number);
@@ -9,3 +10,4 @@ console.log(typeof ecall);
 console.log(typeof null);
 console.log(typeof a);
 console.log(typeof console.log);
+console.log(typeof TELL);
